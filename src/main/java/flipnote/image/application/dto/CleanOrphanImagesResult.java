1b24a741cae25e139e4cfbd1497d374cf37b1840
stoneTiger0912 <seokbeom0912@gmail.com>
2026-02-03 18:12:25 +0900

    Delete: 필요없는 클래스 삭제

--- src/main/java/flipnote/image/application/dto/CleanOrphanImagesResult.java
+++
@@ -1,5 +0,0 @@
-package flipnote.image.application.dto;
-
-public record CleanOrphanImagesResult(int processedBatches, int s3DeletedCount, int dbDeletedCount, int s3FailedCount,
-									  int dbFailedCount) {
-}
